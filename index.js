1c4844be6a4a9c8825fc1edde1993c949911e159
sirna17 <raharimanantsoamanitriniainaer@gmail.com>
2023-09-27 12:08:09 +0400

    salut

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log("toto");
+
+// import du paquet mysql pour pouvoir l'utilisation ici 
+import mysql from "mysql"
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'restful_api'
+});
+ 
+connection.connect((err) => {
+        if(err) throw err
+        console.log("ok");
+});
