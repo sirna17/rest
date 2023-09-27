cc6dff4b294d146a0b7958ebfd7bbdfa526f141b
sirna17 <raharimanantsoamanitriniainaer@gmail.com>
2023-09-27 15:35:51 +0400

    finish

--- index.js
+++ index.js
@@ -13,3 +13,45 @@ connection.connect((err) => {
         if(err) throw err
         console.log("ok");
 });
+
+function getAllUsers() {
+        connection.query('SELECT * FROM users', (error, results) => {
+                if (error) throw error;
+
+                for (let i = 0; i < results.length; i++) {
+                        console.log('#ID : ', results[i].id);
+                        console.log('Nom : ', results[i].nom);
+                        console.log('Email : ', results[i].email);
+                        console.log('Password : ', results[i].password);
+                }
+        });
+
+        connection.end();
+}
+
+// getAllUsers();
+
+function getUserId () {
+        connection.query('SELECT id FROM users', (error, results) => {
+                if (error) throw error;
+
+                for (let i = 0; i < results.length; i++) {
+                        console.log('#ID : ', results[i].id);
+                }
+        })
+};
+// getUserId();
+
+function  getTaskFromNameUser () {
+        connection.query('SELECT nom FROM users', (error, results) => {
+                if (error) throw error;
+
+                for (let i = 0; i < results.length; i++) {
+                        console.log('Nom : ', results[i].nom);
+                }
+        })
+};
+getTaskFromNameUser ();
+      
+        
+      
