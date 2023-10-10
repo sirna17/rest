1e5c0d9cbf949cd56effcdadd90916e15f1522b6
sirna17 <raharimanantsoamanitriniainaer@gmail.com>
2023-10-10 16:19:05 +0400

    ajout d'un taskControlleur

---
+++ controlleur/userControlleur.js
@@ -0,0 +1,27 @@
+
+import {connection} from "../db.js"
+export function getAllUsers(req,res) {
+        connection.query('SELECT * FROM users', (error, results) => {
+                if (error) throw error;
+
+                for (let i = 0; i < results.length; i++) {
+                        console.log('#ID : ', results[i].id);
+                        console.log('Nom : ', results[i].nom);
+                        console.log('Email : ', results[i].email);
+                        console.log('Password : ', results[i].password);
+                }
+                res.send(results)
+        });
+
+        connection.end();
+}
+
+export function getUserId () {
+        connection.query('SELECT id FROM users', (error, results) => {
+                if (error) throw error;
+
+                for (let i = 0; i < results.length; i++) {
+                        console.log('#ID : ', results[i].id);
+                }
+        })
+};
---
+++ routeur/userRouteur.js
@@ -0,0 +1,13 @@
+import  express  from "express";
+
+// creation du routeur express
+export const userRouteur = express.Router()
+
+//importer les controlleur
+import { getAllUsers } from "./../controlleur/userControlleur.js";
+
+//route /users pour obtenir les liste des utilisateurs
+userRouteur.get('/users',(req,res) => {
+        getAllUsers(req,res)
+})
+
